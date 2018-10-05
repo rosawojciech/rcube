8bfde498d1c6dd277cced1cf601e60a18b9e4cca
KMS131C4\wojci <w.rosa@pollub.pl>
2018-10-05 13:35:02 +0200

    Modifying plot functions - non return functions, and modifying examples

--- R/twistCube.R
+++ R/twistCube.R
@@ -25,20 +25,18 @@
 #' plot3dCube(c)
 #' # The same pattern using pipe %>% from magrittr package
 #' require(magrittr)
-#' c <- createCube() %>% twistCube(positions[21,"moves"]) %>% plot3dCube()
+#' createCube() %>% twistCube(positions[21,"moves"]) %>% plot3dCube()
 #' # Rubik's Revenge
-#' c <- createCube(N = 4) %>% plot3dCube()
-#' # Rotating only one edge, which is not allowed on a classic cube:
-#' c %>% twistCube(positions[30,"moves"]) %>% plot3dCube()
+#' createCube(N = 4) %>% plot3dCube()
 #' # Creating Professor's Cube
-#' c <- createCube(N = 5) %>% plot3dCube()
+#' createCube(N = 5) %>% plot3dCube()
 #' # Rotating and moving edges:
-#' c %>% twistCube("(u3RUrFrfRU3)x12") %>% plot3dCube()
+#' createCube(N = 5) %>% twistCube("(u3RUrFrfRU3)x12") %>% plot3dCube()
 #' # Moving and rotating edges part 2:
-#' c <- createCube(5) %>% twistCube("((R1:2)x2 BBUU (L1:2)x2 UU rr2
+#' createCube(5) %>% twistCube("((R1:2)x2 BBUU (L1:2)x2 UU rr2
 #' UU RR2 UUFF RR2 FF ll2 BB (R1:2)x2 )x2 dd") %>% plot3dCube()
 #' # Hearts pattern on a cube sized 13x13x13:
-#' c <- createCube(13) %>% twistCube("OP U2
+#' createCube(13) %>% twistCube("OP U2
 #' l4:5 R4:5 u2 L4:5 r4:5 U3
 #' l3:6 R3:6 u3 L3:6 r3:6 U4
 #' l2:4 R2:4 l6:8 u4 L2:4 r2:4 L6:8 U5
@@ -50,11 +48,11 @@
 #' L5:9 d3 l6:8 D3
 #' L6:8 d2 l7 D2 L7") %>% plot3dCube()
 #' # Creating octa cube
-#' c <- createCube(N = 4, mode = "octa") %>%  plot3dCube()
+#' createCube(N = 4, mode = "octa") %>% plot3dCube(c)
 #' # Rotating centers which is not visible on a classic cube (URL algorithm):
-#' c %>% twistCube("(URL uurl)x2") %>% plot3dCube()
+#' createCube(N = 4, mode = "octa") %>% twistCube("(URLuurl)x2") %>% plot3dCube()
 #' # Creating void cube 8x8x8
-#' c <- createCube(N = 8,mode = "void") %>% plot3dCube()
+#' createCube(N = 8,mode = "void") %>% plot3dCube()
 #' @import magrittr
 #' @export
 twistCube <- function(cube,moves = "", times = 1){
